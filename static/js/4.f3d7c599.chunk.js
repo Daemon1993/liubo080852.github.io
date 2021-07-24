38e70fb67b77a458ed529c9a968cc2ceaa9883b5
Daemon <965204390@qq.com>
2021-07-24 21:15:49 +0800

    update

---
+++ static/js/4.f3d7c599.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[4],{49:function(t,n,e){"use strict";e.r(n),e.d(n,"default",(function(){return o}));var r=e(19),a=e(20),c=e(22),u=e(21),p=e(0),i=e.n(p),s=e(3),o=function(t){Object(c.a)(e,t);var n=Object(u.a)(e);function e(){return Object(r.a)(this,e),n.apply(this,arguments)}return Object(a.a)(e,[{key:"render",value:function(){return Object(s.jsx)("h1",{children:"SelfU"})}}]),e}(i.a.Component)}}]);
+//# sourceMappingURL=4.f3d7c599.chunk.js.map
