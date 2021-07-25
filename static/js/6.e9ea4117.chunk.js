5572ca030793139c77a0ebf67aa06adf634dae05
Daemon <965204390@qq.com>
2021-07-25 12:11:41 +0800

    update

---
+++ static/js/6.e9ea4117.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[6],{293:function(t,n,e){"use strict";e.r(n),e.d(n,"default",(function(){return o}));var c=e(19),r=e(20),a=e(22),u=e(21),p=e(0),i=e.n(p),s=e(3),o=function(t){Object(a.a)(e,t);var n=Object(u.a)(e);function e(){return Object(c.a)(this,e),n.apply(this,arguments)}return Object(r.a)(e,[{key:"render",value:function(){return Object(s.jsx)("h1",{children:"Tech"})}}]),e}(i.a.Component)}}]);
+//# sourceMappingURL=6.e9ea4117.chunk.js.map
